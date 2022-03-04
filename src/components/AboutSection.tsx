85ebd578d9b2edff042d1782053d7563919ef5e4
kitkovsky <okitkowski114@gmail.com>
2022-03-04 12:57:34 +0100

    feat: start styling the about section

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,33 +1,34 @@
 import React from "react";
-import home1 from "../img/home1.png";
+import home1 from "../img/home1.jpg";
+import { About, Description, Hide, Image } from "../styles/Styles";
 
 const AboutSection: React.FC = () => {
   return (
-    <div>
-      <div className="description">
+    <About>
+      <Description>
         <div className="title">
-          <div className="hide">
+          <Hide>
             <h2>We work to make</h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>
               your <span>dreams</span> come
             </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>true</h2>
-          </div>
+          </Hide>
         </div>
         <p>
           Contact us for any photography of videography ideas that you have. We
           have professionals ready to tackle your project.
         </p>
         <button>Contact us</button>
-      </div>
-      <div className="image">
+      </Description>
+      <Image>
         <img src={home1} alt="man holding a camera" />
-      </div>
-    </div>
+      </Image>
+    </About>
   );
 };
 
---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,64 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    background: rgb(20, 20, 20);
+  }
+
+  button {
+    font-weight: bold;
+    font-size: 2.3rem;
+    cursor: pointer;
+    padding: 1rem 2rem;
+    border: 3px solid #23d997;
+    background: transparent;
+    color: white;
+    transition: all 0.3s ease;
+    &:hover {
+      background: #23d997;
+    }
+    
+  }
+
+  h2 {
+    font-weight: lighter;
+    font-size: 6rem;
+  }
+
+  h3 {
+    font-size: 2.3rem;
+    color: white;
+  }
+
+  h4 {
+    font-weight: bold;
+  }
+
+  span {
+    font-weight: bold;
+    color: #23d997;
+  }
+
+  a {
+    font-size: 2.3rem;
+  }
+
+  p {
+    padding: 3rem 0rem;
+    color: #ccc;
+    font-size: 2.3rem;
+    line-height: 150%;
+  }
+`;
+
+export default GlobalStyle;
