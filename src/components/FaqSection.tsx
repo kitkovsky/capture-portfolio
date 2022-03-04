4c6492d5f26e3dadc6608b963b9bbfb24a42fb8a
kitkovsky <okitkowski114@gmail.com>
2022-03-04 13:24:21 +0100

    feat: faq and services styling

--- src/components/FaqSection.tsx
+++ src/components/FaqSection.tsx
@@ -1,8 +1,47 @@
 import React from "react";
+import styled from "styled-components";
+import { About } from "../styles/Styles";
+
+const Faq = styled(About)`
+  display: block;
+
+  span {
+    display: block;
+  }
+
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+
+  h4 {
+    font-size: 3rem;
+  }
+
+  .faq-line {
+    background: #cccccc;
+    height: 0.4rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+
+  .question {
+    padding: 4rem 0rem;
+    cursor: pointer;
+  }
+
+  .answer {
+    padding: 2.5rem 0rem;
+
+    p {
+      padding: 1.3rem 0rem;
+    }
+  }
+`;
 
 const FaqSection: React.FC = () => {
   return (
-    <div className="faq">
+    <Faq>
       <h2>
         Any questions? <span>FAQ</span>
       </h2>
@@ -10,31 +49,43 @@ const FaqSection: React.FC = () => {
         <h4>How do I start?</h4>
         <div className="answer">
           <p>Amet quam illum consequatur sed.</p>
-          <p>Lorem labore at ratione id consequuntur. Tempora fugit odio quis.</p>
+          <p>
+            Lorem labore at ratione id consequuntur. Tempora fugit odio quis.
+          </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Daily schedule</h4>
         <div className="answer">
           <p>Amet quam illum consequatur sed.</p>
-          <p>Lorem labore at ratione id consequuntur. Tempora fugit odio quis.</p>
+          <p>
+            Lorem labore at ratione id consequuntur. Tempora fugit odio quis.
+          </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Diferrent payment methods</h4>
         <div className="answer">
           <p>Amet quam illum consequatur sed.</p>
-          <p>Lorem labore at ratione id consequuntur. Tempora fugit odio quis.</p>
+          <p>
+            Lorem labore at ratione id consequuntur. Tempora fugit odio quis.
+          </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>What products do you offer</h4>
         <div className="answer">
           <p>Amet quam illum consequatur sed.</p>
-          <p>Lorem labore at ratione id consequuntur. Tempora fugit odio quis.</p>
+          <p>
+            Lorem labore at ratione id consequuntur. Tempora fugit odio quis.
+          </p>
         </div>
+        <div className="faq-line"></div>
       </div>
-    </div>
+    </Faq>
   );
 };
 
