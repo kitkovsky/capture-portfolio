f11c75827a2dd7fdebdb8209e06fed4c5ff26f72
kitkovsky <okitkowski114@gmail.com>
2022-03-05 20:02:59 +0100

    feat: contact page details and animation

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,18 +1,77 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { pageAnimation } from "../global/animations";
+import { pageAnimation, titleAnimation } from "../global/animations";
+import styled from "styled-components";
+
+const StyledContact = styled(motion.div)`
+  padding: 5rem 10rem;
+  color: #353535;
+  min-height: 90vh;
+
+  h2 {
+    color: white;
+  }
+`;
+
+const Title = styled.div`
+  margin-bottom: 4rem;
+  color: black;
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
+
+const Social = styled(motion.div)`
+  display: flex;
+  align-items: center;
+
+  h2 {
+    margin: 2rem;
+  }
+`;
+
+const Circle = styled.div`
+  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+  background-color: palegreen;
+`;
 
 const Contact: React.FC = () => {
   return (
-    <motion.div
+    <StyledContact
       variants={pageAnimation}
       initial="initial"
       animate="animate"
       exit="exit"
-      style={{ background: "#fff" }}
     >
-      <h1>contact</h1>
-    </motion.div>
+      <Title>
+        <Hide>
+          <motion.h2 variants={titleAnimation}>Get in touch.</motion.h2>
+        </Hide>
+      </Title>
+      <div>
+        <Hide>
+          <Social variants={titleAnimation}>
+            <Circle></Circle>
+            <h2>Send us a message.</h2>
+          </Social>
+        </Hide>
+        <Hide>
+          <Social variants={titleAnimation}>
+            <Circle></Circle>
+            <h2>Send an email.</h2>
+          </Social>
+        </Hide>
+        <Hide>
+          <Social variants={titleAnimation}>
+            <Circle></Circle>
+            <h2>Our social media.</h2>
+          </Social>
+        </Hide>
+      </div>
+    </StyledContact>
   );
 };
 
